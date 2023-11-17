3561af70c58599d9e322c52df0453a741af42332
agistarakha <agistarakhasnc@gmail.com>
2023-11-17 13:51:57 +0700

    Init: fluent UI library
    - Install fluent UI
    - Add some placeholder fluent UI code for initial testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { TextField } from "@fluentui/react/lib/TextField";
+import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
+
 function App() {
   return (
     <>
-      <div className="bg-red-700 text-8xl text-center">Hello</div>
+      <div className="text-center">Task Manager</div>
+      <div className="px-2">
+        <TextField label="Title" className="w-8/12" />
+        <TextField label="Description" multiline rows={3} className="w-8/12" />
+        <DefaultButton text="Submit" />
+      </div>
     </>
   );
 }
