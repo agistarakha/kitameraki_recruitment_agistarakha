fef8e32f7d3ee4437dc3487e05c1350dfc797791
agistarakha <agistarakhasnc@gmail.com>
2023-11-18 21:46:45 +0700

    Add: api to add task
    - Added POST endpoint '/api/tasks' to handle task creation
    - Implemented logic to store new tasks in memory using json file
    - Utilized appropriate HTTP methods and status codes

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -4,11 +4,12 @@ import * as fs from "fs";
 import * as path from "path";
 
 const taskSchema = z.object({
-  id: z.number().min(1),
   title: z.string().min(1),
   description: z.string().optional(),
 });
-type Task = z.infer<typeof taskSchema>;
+type Task = z.infer<typeof taskSchema> & {
+  id: number;
+};
 
 const dataFilePath = path.join(__dirname, "../../data/tasks.json");
 
@@ -22,7 +23,33 @@ const readTasksFromFile = (): Task[] => {
   }
 };
 
+const writeTasksToFile = (task: Task[]) => {
+  try {
+    fs.writeFileSync(dataFilePath, JSON.stringify(task, null, 2), "utf-8");
+  } catch (error) {
+    console.error("Error writing tasks to file", error);
+  }
+};
+
 export const getAllTasks = (req: Request, res: Response) => {
   const tasks = readTasksFromFile();
   res.json(tasks);
+};
+
+export const addTask = (req: Request, res: Response) => {
+  try {
+    const { title, description } = taskSchema.parse(req.body);
+    const tasks = readTasksFromFile();
+    const newTask: Task = {
+      id: tasks.length + 1,
+      title,
+      description,
+    };
+    tasks.push(newTask);
+    writeTasksToFile(tasks);
+    res.status(201).json(newTask);
+  } catch (error) {
+    console.error("Error adding task", error);
+    res.status(400).json({ error: "Invalid data provided" });
+  }
 };
--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,11 +1,7 @@
 import express from "express";
 export const router = express.Router();
-import { getAllTasks } from "../controllers/taskController";
+import { getAllTasks, addTask } from "../controllers/taskController";
 
 router.get("/", getAllTasks);
 
-router.post("/", (req, res) => {
-  const data = req.body;
-  console.log(data);
-  res.status(201).json({ message: "Success adding task" });
-});
+router.post("/", addTask);
